6c74df8d451185d818d33993f24ca2f34e79c7b2
Steven Shyaka <sshya024@uottawa.ca>
2026-03-28 16:51:10 -0400

    feat: Migrating from CopilotChat to Sidekick for AI integration.

--- init.lua
+++ init.lua
@@ -613,6 +613,7 @@ require('lazy').setup({
         pyright = {},
         terraformls = {},
         yamlls = {},
+        copilot = {},
         -- gopls = {},
         -- rust_analyzer = {},
         --
@@ -721,7 +722,6 @@ require('lazy').setup({
     event = 'VimEnter',
     version = '1.*',
     dependencies = {
-      'fang2hou/blink-copilot',
       -- Snippet Engine
       {
         'L3MON4D3/LuaSnip',
@@ -791,15 +791,7 @@ require('lazy').setup({
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'copilot' },
-        providers = {
-          copilot = {
-            name = 'copilot',
-            module = 'blink-copilot',
-            score_offset = 100,
-            async = true,
-          },
-        },
+        default = { 'lsp', 'path', 'snippets' },
       },
 
       snippets = { preset = 'luasnip' },
@@ -895,7 +887,25 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'python',
+        'javascript',
+        'yaml',
+        'groovy',
+        'java',
+        'terraform',
+      }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
